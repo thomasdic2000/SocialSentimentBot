f62292133034e0668080995ce66320866b594514
thomasdic2000 <thomasdic2000@gmail.com>
2020-08-13 00:02:17 -0500

    new image

--- src/secret.py
+++ src/secret.py
@@ -3,8 +3,8 @@
 # https://www.reddit.com/prefs/apps #
 
 
-client_id = "xyz"
+client_id = "JrfCE2Ajg94c1w"
 
-client_secret = "abc"
+client_secret = "-H1R8_19wfHbGVx7wI4JhP8YnXM"
 
-user_agent = "MarketTrack"
+user_agent = "SocialSentimentApp"
